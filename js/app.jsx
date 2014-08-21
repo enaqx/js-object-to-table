f7fb788b33bd4f1293b0b597521dc6dd5212a91a
Nick Raienko <enaqxx@gmail.com>
2014-08-21 12:03:27 +0300

    Draw Table component

--- js/app.jsx
+++ js/app.jsx
@@ -1,5 +1,29 @@
 /** @jsx React.DOM */
+
+var Table = React.createClass({
+  getInitialState: function() {
+    return {data: this.props.data};
+  },
+  render: function () {
+    return (
+      React.DOM.table(null, React.DOM.tbody(null,
+        this.state.data.map(function (row) {
+          return (
+            React.DOM.tr(null, 
+              row.map(function (cell) {
+                return React.DOM.td(null, cell);
+              })
+            )
+          );
+        })
+      ))
+    );
+  }
+});
+
+var data = [[1,2,3],[4,5,6],[7,8,9]];
+
 React.renderComponent(
-  React.DOM.h1(null, "Hello World"),
-  document.getElementById('content')
+  Table({data: data}),
+  document.getElementById('table')
 );
--- server.js
+++ server.js
@@ -5,17 +5,20 @@
  */
 var fs = require('fs');
 var koa = require('koa');
+var favicon = require('koa-favicon');
 var serve = require('koa-static');
 
 /**
  * Configuration
  */
 var port = 3000;
+var icon = '/img/favicon.ico';
 
 /**
  * Server
  */
 var app = module.exports  = koa();
 app.use(serve(__dirname));
+app.use(favicon(__dirname + icon));
 app.listen(port);
 console.log('Server started, listening on port: ' + port);
